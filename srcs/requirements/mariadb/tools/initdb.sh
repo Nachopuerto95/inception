94e511553043ad35a0af77b6d753b3deafab4d5e
nacho <nachopuerto95@gmail.com>
2025-09-22 12:48:00 +0200

    v2

--- srcs/requirements/mariadb/tools/initdb.sh
+++ srcs/requirements/mariadb/tools/initdb.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+set -e
 
-# Cambiar contraseña root
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysqld_safe --datadir=/var/lib/mysql &
 
-# Aplicar cambios
-mysql -e "FLUSH PRIVILEGES;"
+until mysqladmin ping --silent; do
+    echo "Esperando a que MariaDB esté lista..."
+    sleep 2
+done
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysql -u root <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+    CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
+EOSQL
 
-exec mysqld_safe
+
+mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+exec mysqld_safe --datadir=/var/lib/mysql
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 sleep 10
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-	wp config create	--allow-root \
-									--dbname=$SQL_DATABASE \
-									--dbuser=$SQL_USER \
-									--dbpass=$SQL_PASSWORD \
-									--dbhost=mariadb:3306 --path='/var/www/wordpress'
+mkdir -p /run/php && chown -R www-data:www-data /run/php
+
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	wp config create --allow-root \
+		--dbname=$SQL_DATABASE \
+		--dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD \
+		--dbhost=mariadb:3306 --path='/var/www/wordpress'
 fi
+
+chown -R www-data:www-data /var/www/wordpress
 
 /usr/sbin/php-fpm7.3 -F
