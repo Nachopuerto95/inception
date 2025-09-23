70b1de85aa65823d342168c78133b66ef9ab0df5
nacho <nachopuerto95@gmail.com>
2025-09-23 11:18:58 +0200

    asd

--- srcs/requirements/mariadb/tools/initdb.sh
+++ srcs/requirements/mariadb/tools/initdb.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-set -e
+service mysql start;
 
-mysqld_safe --datadir=/var/lib/mysql &
-
-until mysqladmin ping --silent; do
-    echo "Esperando a que MariaDB esté lista..."
-    sleep 2
-done
-
-mysql -u root <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
-    CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
-    FLUSH PRIVILEGES;
-EOSQL
-
-
-mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
-exec mysqld_safe --datadir=/var/lib/mysql
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"		
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"				
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
+				
