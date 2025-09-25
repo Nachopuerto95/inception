ee40dc1e967bcbac8fff011e94a041af52ea7c85
nacho <nachopuerto95@gmail.com>
2025-09-25 07:21:15 +0200

    asd

--- srcs/requirements/mariadb/tools/initdb.sh
+++ srcs/requirements/mariadb/tools/initdb.sh
@@ -11,17 +11,17 @@ done
 
 # Inicializar la base de datos si aún no existe
 if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
-    mysql -u "root" -p"${SQL_ROOT_PASSWORD}" <<EOF
+    mysql -u "${ROOT_USR}" -p"${SQL_ROOT_PASSWORD}" <<EOF
 CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+ALTER USER '${ROOT_USR}'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
 EOF
 fi
 
 # Apagar la instancia en segundo plano
-mysqladmin -u "root" -p"${SQL_ROOT_PASSWORD}" shutdown
+mysqladmin -u "${ROOT_USR}" -p"${SQL_ROOT_PASSWORD}" shutdown
 
 # Iniciar MariaDB en primer plano para mantener el contenedor activo
 exec mysqld --datadir=/var/lib/mysql --user=mysql
